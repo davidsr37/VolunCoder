c1dc23d87be2366f619edfc5e81d96cc635a448c
davidsr37 <davidsamuelrogers@gmail.com>
2015-02-25 12:25:28 -0800

    Org events and org messages VC's set, working on transition from "add" bar button item to "add message" view and "add event" view.

---
+++ VolunCode/OrgMessagesViewController.h
@@ -0,0 +1,14 @@
+//
+//  OrgMessagesViewController.h
+//  VolunCode
+//
+//  Created by David Rogers on 2/25/15.
+//  Copyright (c) 2015 Clint Akins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "OrgAddMessage.h"
+
+@interface OrgMessagesViewController : UITableViewController
+
+@end
