28c99e25bc8be75d98981b9c7db62edab7eef0e0
davidsr37 <davidsamuelrogers@gmail.com>
2015-02-26 16:25:10 -0800

    More VC's and more outlets.

---
+++ EventCell.m
@@ -0,0 +1,23 @@
+//
+//  EventCell.m
+//  VolunCode
+//
+//  Created by David Rogers on 2/26/15.
+//  Copyright (c) 2015 Clint Akins. All rights reserved.
+//
+
+#import "EventCell.h"
+
+@implementation EventCell
+
+- (void)awakeFromNib {
+    // Initialization code
+}
+
+- (void)setSelected:(BOOL)selected animated:(BOOL)animated {
+    [super setSelected:selected animated:animated];
+
+    // Configure the view for the selected state
+}
+
+@end
---
+++ VolunCode/Events.h
@@ -0,0 +1,13 @@
+//
+//  Events.h
+//  VolunCode
+//
+//  Created by David Rogers on 2/26/15.
+//  Copyright (c) 2015 Clint Akins. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface Events : UITableViewCell
+
+@end
